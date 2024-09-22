a208ceba0a1628a8ed5de8015dda246383dab47f
kp-2002 <kaivalyaep22@gmail.com>
2024-09-22 11:47:59 +0000

    4th push

--- rtl/bus_interface_unit/seg_reg_file.v
+++ rtl/bus_interface_unit/seg_reg_file.v
@@ -18,19 +18,28 @@ module seg_reg_file(
 	);
 
 	reg [15:0] code_seg_reg;
+	//contains the base or start of the current code segment
+	//offset by the 16 bit instruction pointer register to obtain the address of the next instruction 
 	reg [15:0] data_seg_reg;
+	//points to the current data segment from where most operands are fetched
+	//offset by the 16 bit source index or destination index registers or by a 16 bit displacement
 	reg [15:0] extra_seg_reg;
+	//points to the extra segment where additional data is stored
+	//offset by the 16 bit destination index register
 	reg [15:0] stack_seg_reg;
+	//points to the current stack
+	//offset by the 16 bit stack pointer and base pointer registers
 	
 	always @(posedge clk or posedge rst) begin
 		if(rst) begin
-			//do reset
+			//reset operation
 			code_seg_reg  <= 16'b0;
 			data_seg_reg  <= 16'b0;
 			extra_seg_reg <= 16'b0; 
 			stack_seg_reg <= 16'b0; 
 		end
 		else begin
+			//write operation
 			if(wr_en) begin
 				case(wr_sel)
 					2'b00: code_seg_reg  <= wr_data;
@@ -39,6 +48,7 @@ module seg_reg_file(
 					2'b11: stack_seg_reg <= wr_data; 
 				endcase
 			end
+			//read operation
 			if(rd_en) begin
 				case(rd_sel)
 					2'b00: rd_data <= code_seg_reg;
